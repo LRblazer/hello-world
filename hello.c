01a3b53c413da7ee9a9957b432e7a089558cdcb5
xuxiaohei <986247989@qq.com>
2020-07-22 21:27:45 +0800

    hello

---
+++ hello.c
@@ -0,0 +1,18 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2020 BIG WORLD
+ *                  All rights reserved.
+ *
+ *       Filename:  hello.c
+ *    Description:  This file 
+ *                 
+ *        Version:  1.0.0(2020年07月22日)
+ *         Author:  xuxiaohei <1411032148@qq.com>
+ *      ChangeLog:  1, Release initial version on "2020年07月22日 21时27分04秒"
+ *                 
+ ********************************************************************************/
+
+int main(int argc, char *argv[])
+{
+    printf("hello github\n");
+    return 0;
+}
